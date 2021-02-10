0db92ccd9c8228eddc53339ee0cd17c8d3722ca3
jmlivingston <jmlivingston@users.noreply.github.com>
2021-02-10 18:09:25 +0000

    deploy: e6396de19074bf7186a4b61fdd75a8f1201dd41b

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   buildOptions: {
-    baseUrl: '',
+    baseUrl: '/snowpack-multi-framework',
   },
   plugins: ['@snowpack/plugin-svelte', '@snowpack/plugin-vue'],
 }
