8e76f64457c6e6d9b2cd9ea2cd22f2633d9e12df
John Livingston <jmlivingston@gmail.com>
2021-02-10 08:32:27 -0800

    Updates

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,15 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
-  buildOptions: { baseUrl: '/snowpack-multi-framework' },
+  // mount and buildOptions optional for deploying to GitHub Pages
+  // mount: {
+  //   public: '/',
+  //   src: '/_dist_',
+  // },
+  // buildOptions: {
+  //   baseUrl: '',
+  // },
+  buildOptions: {
+    baseUrl: '/snowpack-multi-framework',
+  },
   plugins: ['@snowpack/plugin-svelte', '@snowpack/plugin-vue'],
 }
